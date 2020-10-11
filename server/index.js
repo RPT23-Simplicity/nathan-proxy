272bb4282c1b11441132b8df6448549224ce458a
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2020-10-10 19:16:27 -0700

    refectored index.js to handle product & style id in url

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../public/dist'));
 
 app.listen(5000, () => {
     console.log('Listening on port 5000');
 });
+
+app.get('/:productId/:styleId', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/dist/index.html'));
+})
 
